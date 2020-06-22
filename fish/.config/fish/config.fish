acad603b1b9ece1f10acf1812f82df7a931686a2
David Pinheiro <davepinh@gmail.com>
2020-06-21 21:21:47 -0300

    Fish shell, new scripts and openbox

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+fish_vi_key_bindings
+
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
+set fish_greeting
+
+source ~/.aliases
+
+set -gx EDITOR vim
+
+set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.bin
