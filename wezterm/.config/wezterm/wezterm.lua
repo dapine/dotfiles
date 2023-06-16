6bbf5bdf75c79b09f4a22e5455c9d6c343bd53fd
Breno Pinheiro <davepinh@gmail.com>
2023-06-16 14:06:47 -0300

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,20 +2,24 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = {}
 
+local cs = 'Catppuccin Mocha'
 local slash = package.config:sub(1, 1)
 local my_os = slash == '/' and 'unix' or 'windows'
+
+local scheme = wezterm.get_builtin_color_schemes()[cs]
 
 config = {
   font = wezterm.font("Fira Code"),
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   enable_scroll_bar = true,
   use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
   inactive_pane_hsb = {
     saturation = 0.5,
     brightness = 0.1,
   },
-  color_scheme = 'Catppuccin Mocha',
+  color_scheme = cs,
 }
 
 if my_os == 'windows' then
@@ -39,7 +43,6 @@ config.keys = {
   { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down' },
   { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up' },
   { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right' },
-  { key = 'p', mods = 'ALT', action = act.ShowLauncher },
   { key = 'r', mods = 'ALT', action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false } },
   { key = 'z', mods = 'ALT', action = act.TogglePaneZoomState },
   { key = '[', mods = 'ALT', action = act.ActivateTabRelative(-1) },
@@ -57,5 +60,24 @@ config.key_tables = {
 	{ key = 'q', action = 'PopKeyTable' },
   }
 }
+
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  if title and #title > 0 then
+    return title
+  end
+  return tab_info.active_pane.title
+end
+
+wezterm.on('format-tab-title', function(tab)
+  local active_pane_zoomed = tab.active_pane.is_zoomed and '(Z)' or ''
+  local title = tab_title(tab)
+  if tab.active_pane.is_zoomed then
+	return {
+	  { Foreground = { Color = scheme.brights[4] } },
+      { Text = ' ' .. title .. ' ' .. active_pane_zoomed .. ' ' },
+	}
+  end
+end)
 
 return config
