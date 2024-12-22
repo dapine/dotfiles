59effedbfad3cb05097030b4f9374adf07387952
Breno Pinheiro <davepinh@gmail.com>
2024-12-22 12:48:41 -0300

    Format wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,83 +1,83 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 
-local cs = 'Catppuccin Mocha'
+local cs = "Catppuccin Mocha"
 local slash = package.config:sub(1, 1)
-local my_os = slash == '/' and 'unix' or 'windows'
+local my_os = slash == "/" and "unix" or "windows"
 
 local scheme = wezterm.get_builtin_color_schemes()[cs]
 
 config = {
-  font = wezterm.font("Fira Code"),
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-  enable_scroll_bar = true,
-  use_fancy_tab_bar = false,
-  tab_bar_at_bottom = true,
-  hide_tab_bar_if_only_one_tab = true,
-  inactive_pane_hsb = {
-    saturation = 0.5,
-    brightness = 0.1,
-  },
-  color_scheme = cs,
+	font = wezterm.font("Fira Code"),
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	enable_scroll_bar = true,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	inactive_pane_hsb = {
+		saturation = 0.5,
+		brightness = 0.1,
+	},
+	color_scheme = cs,
 }
 
-if my_os == 'windows' then
-  config.wsl_domains = {
-    {
-      name = 'WSL:Ubuntu',
-      distribution = 'Ubuntu',
-      username = "d",
-      default_cwd = "~",
-      default_prog = {"bash"},
-    },
-  }
-  config.default_domain = 'WSL:Ubuntu'
+if my_os == "windows" then
+	config.wsl_domains = {
+		{
+			name = "WSL:Ubuntu",
+			distribution = "Ubuntu",
+			username = "d",
+			default_cwd = "~",
+			default_prog = { "bash" },
+		},
+	}
+	config.default_domain = "WSL:Ubuntu"
 end
 
 config.keys = {
-  { key = 't', mods = 'ALT', action = act.SpawnTab 'CurrentPaneDomain' },
-  { key = '\\', mods = 'ALT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = '-', mods = 'ALT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left' },
-  { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down' },
-  { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up' },
-  { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right' },
-  { key = 'r', mods = 'ALT', action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false } },
-  { key = 'z', mods = 'ALT', action = act.TogglePaneZoomState },
-  { key = '[', mods = 'ALT', action = act.ActivateTabRelative(-1) },
-  { key = ']', mods = 'ALT', action = act.ActivateTabRelative(1) },
+	{ key = "t", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "\\", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+	{ key = "r", mods = "ALT", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+	{ key = "z", mods = "ALT", action = act.TogglePaneZoomState },
+	{ key = "[", mods = "ALT", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "ALT", action = act.ActivateTabRelative(1) },
 }
 
 config.key_tables = {
-  resize_pane = {
-    { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
-	{ key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
-	{ key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
-	{ key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
-	{ key = 'Escape', action = 'PopKeyTable' },
-	{ key = 'Enter', action = 'PopKeyTable' },
-	{ key = 'q', action = 'PopKeyTable' },
-  }
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+		{ key = "q", action = "PopKeyTable" },
+	},
 }
 
 function tab_title(tab_info)
-  local title = tab_info.tab_title
-  if title and #title > 0 then
-    return title
-  end
-  return tab_info.active_pane.title
+	local title = tab_info.tab_title
+	if title and #title > 0 then
+		return title
+	end
+	return tab_info.active_pane.title
 end
 
-wezterm.on('format-tab-title', function(tab)
-  local active_pane_zoomed = tab.active_pane.is_zoomed and '(Z)' or ''
-  local title = tab_title(tab)
-  if tab.active_pane.is_zoomed then
-	return {
-	  { Foreground = { Color = scheme.brights[4] } },
-      { Text = ' ' .. title .. ' ' .. active_pane_zoomed .. ' ' },
-	}
-  end
+wezterm.on("format-tab-title", function(tab)
+	local active_pane_zoomed = tab.active_pane.is_zoomed and "(Z)" or ""
+	local title = tab_title(tab)
+	if tab.active_pane.is_zoomed then
+		return {
+			{ Foreground = { Color = scheme.brights[4] } },
+			{ Text = " " .. title .. " " .. active_pane_zoomed .. " " },
+		}
+	end
 end)
 
 return config
