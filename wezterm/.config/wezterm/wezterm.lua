f022c87b27e9911a986b8553ee1adc15435ec351
Breno Pinheiro <davepinh@gmail.com>
2026-04-25 22:47:18 -0300

    Add auto dark mode to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,90 +5,108 @@ local config = {}
 local cs = "Modus-Operandi"
 local scheme = wezterm.get_builtin_color_schemes()[cs]
 
+local function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+    if appearance:find 'Dark' then
+        cs = "Jellybeans"
+    else
+        cs = "Modus-Operandi"
+    end
+    scheme = wezterm.get_builtin_color_schemes()[cs]
+    return cs
+end
+
 config = {
-	font = wezterm.font_with_fallback({ "JetBrains Mono", "Noto Sans" }),
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-	enable_scroll_bar = true,
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	inactive_pane_hsb = {
-		saturation = 0.5,
-		brightness = 0.1,
-	},
-	color_scheme = cs,
+    font = wezterm.font_with_fallback({ "JetBrains Mono", "Noto Sans" }),
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+    enable_scroll_bar = true,
+    use_fancy_tab_bar = false,
+    tab_bar_at_bottom = true,
+    hide_tab_bar_if_only_one_tab = true,
+    inactive_pane_hsb = {
+        saturation = 0.5,
+        brightness = 0.1,
+    },
+    color_scheme = scheme_for_appearance(get_appearance()),
+    enable_wayland = false,
 }
 
 if cs == "Modus-Operandi" then
-	config.colors = {
-		scrollbar_thumb = scheme.brights[1],
-		tab_bar = {
-			background = scheme.brights[1],
-			active_tab = {
-				bg_color = scheme.ansi[1],
-				fg_color = "#000000",
-				intensity = "Bold",
-			},
-			inactive_tab = {
-				bg_color = scheme.brights[1],
-				fg_color = "#000000",
-			},
-			new_tab = {
-				bg_color = scheme.brights[1],
-				fg_color = "#000000",
-			},
-		},
-	}
+    config.colors = {
+        scrollbar_thumb = scheme.brights[1],
+        tab_bar = {
+            background = scheme.brights[1],
+            active_tab = {
+                bg_color = scheme.ansi[1],
+                fg_color = "#000000",
+                intensity = "Bold",
+            },
+            inactive_tab = {
+                bg_color = scheme.brights[1],
+                fg_color = "#000000",
+            },
+            new_tab = {
+                bg_color = scheme.brights[1],
+                fg_color = "#000000",
+            },
+        },
+    }
 end
 
 config.keys = {
-	{ key = "t", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "\\", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "-", mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
-	{ key = "r", mods = "ALT", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
-	{ key = "z", mods = "ALT", action = act.TogglePaneZoomState },
-	{ key = "[", mods = "ALT", action = act.ActivateTabRelative(-1) },
-	{ key = "]", mods = "ALT", action = act.ActivateTabRelative(1) },
-	{
-		key = "c",
-		mods = "ALT",
-		action = act.ClearScrollback("ScrollbackAndViewport"),
-	},
+    { key = "t",  mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "\\", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "-",  mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "h",  mods = "ALT", action = act.ActivatePaneDirection("Left") },
+    { key = "j",  mods = "ALT", action = act.ActivatePaneDirection("Down") },
+    { key = "k",  mods = "ALT", action = act.ActivatePaneDirection("Up") },
+    { key = "l",  mods = "ALT", action = act.ActivatePaneDirection("Right") },
+    { key = "r",  mods = "ALT", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+    { key = "z",  mods = "ALT", action = act.TogglePaneZoomState },
+    { key = "[",  mods = "ALT", action = act.ActivateTabRelative(-1) },
+    { key = "]",  mods = "ALT", action = act.ActivateTabRelative(1) },
+    {
+        key = "c",
+        mods = "ALT",
+        action = act.ClearScrollback("ScrollbackAndViewport"),
+    },
 }
 
 config.key_tables = {
-	resize_pane = {
-		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-		{ key = "q", action = "PopKeyTable" },
-	},
+    resize_pane = {
+        { key = "h",      action = act.AdjustPaneSize({ "Left", 1 }) },
+        { key = "j",      action = act.AdjustPaneSize({ "Down", 1 }) },
+        { key = "k",      action = act.AdjustPaneSize({ "Up", 1 }) },
+        { key = "l",      action = act.AdjustPaneSize({ "Right", 1 }) },
+        { key = "Escape", action = "PopKeyTable" },
+        { key = "Enter",  action = "PopKeyTable" },
+        { key = "q",      action = "PopKeyTable" },
+    },
 }
 
 local function tab_title(tab_info)
-	local title = tab_info.tab_title
-	if title and #title > 0 then
-		return title
-	end
-	return tab_info.active_pane.title
+    local title = tab_info.tab_title
+    if title and #title > 0 then
+        return title
+    end
+    return tab_info.active_pane.title
 end
 
 wezterm.on("format-tab-title", function(tab)
-	local active_pane_zoomed = tab.active_pane.is_zoomed and "(Z)" or ""
-	local title = tab_title(tab)
-	if tab.active_pane.is_zoomed then
-		return {
-			{ Foreground = { Color = scheme.brights[4] } },
-			{ Text = " " .. title .. " " .. active_pane_zoomed .. " " },
-		}
-	end
+    local active_pane_zoomed = tab.active_pane.is_zoomed and "(Z)" or ""
+    local title = tab_title(tab)
+    if tab.active_pane.is_zoomed then
+        return {
+            { Foreground = { Color = scheme.brights[4] } },
+            { Text = " " .. title .. " " .. active_pane_zoomed .. " " },
+        }
+    end
 end)
 
 return config
